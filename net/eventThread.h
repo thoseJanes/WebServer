dbdc081345736dc526953e6ecf721d8b3d8b51a0
Eleanor <dlutzhouzijian@163.com>
2025-05-26 10:38:40 +0100

    Try eventLoopThread and eventLoopThreadPool

---
+++ net/eventThread.h
@@ -0,0 +1,68 @@
+#ifndef WEBSERVER_NET_EVENTTHREAD_H
+#define WEBSERVER_NET_EVENTTHREAD_H
+#include "eventLoop.h"
+#include "../process/threadHandler.h"
+#include "../common/condition.h"
+namespace webserver{
+
+//假设start和析构在同一个线程中。仍有可能在loop之前quit。这个问题要如何解决？
+class EventThread{
+public:
+    EventThread()
+    :   thread_(bind(EventThread::threadFunc, this)), 
+        mutex_(), 
+        cond_(mutex_),
+        loop_(NULL),
+    {
+
+    }
+    ~EventThread(){
+    }
+
+    void start(){
+        thread_.start();
+        {
+            MutexLockGuard lock(mutex_);
+            while(!loop_){
+                cond_.wait();
+            }
+        }
+    }
+
+    void join(){
+        if(loop_){
+            loop_->quit();
+            thread_.join();
+        }
+    }
+
+    EventLoop* getLoop(){
+        return loop_;
+    }
+    
+private:
+    void threadFunc(){
+        EventLoop loop;
+        {
+            MutexLockGuard lock(mutex_);
+            loop_ = &loop;
+            cond_.notify();
+        }
+        
+        loop.loop();
+
+        {
+            MutexLockGuard lock(mutex_);
+            loop_ = NULL;
+        }
+    }
+
+    ThreadHandler thread_;
+    MutexLock mutex_;
+    Condition cond_;
+    EventLoop* loop_;
+};
+
+}
+
+#endif
---
+++ net/eventThreadPool.h
@@ -0,0 +1,48 @@
+#ifndef WEBSERVER_NET_EVENTTHREADPOOL_H
+#define WEBSERVER_NET_EVENTTHREADPOOL_H
+#include "eventThread.h"
+namespace webserver{
+
+
+class EventThreadPool{
+public:
+    EventThreadPool(EventLoop* baseLoop, int threadNum):next_(0){
+        baseLoop_.reset(baseLoop);
+    }
+
+    void start(){
+        for(int i=0;i<threadNum_;i++){
+            eventThreads_.emplace_back(new EventThread());
+            eventThreads_[i]->start();
+        }
+    }
+    EventLoop* getNextLoop(){
+        baseLoop_->assertInLoopThread();
+        if(eventThreads_.empty()){
+            return baseLoop_.get();
+        }else{
+            return eventThreads_[next_++]->getLoop();
+        }
+    }
+    EventLoop* getEventLoopByHash(int hashCode){
+        baseLoop_->assertInLoopThread();
+        if(eventThreads_.empty()){
+            return baseLoop_.get();
+        }else{
+            return eventThreads_[hashCode%eventThreads_.size()]->getLoop();
+        }
+    }
+
+    ~EventThreadPool();
+    
+private:
+    int threadNum_;
+    int next_;
+    std::vector<unique_ptr<EventThread>> eventThreads_;
+    unique_ptr<EventLoop> baseLoop_;
+};
+
+
+}
+
+#endif

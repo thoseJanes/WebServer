5d8cf110136a1ab4ad5737dbe8da3aff9407ccde
Eleanor <dlutzhouzijian@163.com>
2025-06-25 14:24:12 +0100

    Adjust directory

--- event/timer.cpp
+++
@@ -1,6 +0,0 @@
-#include "timer.h"
-
-
-using namespace webserver;
-
-std::atomic<int> Timer::id(0);
--- net/connBuffer.cpp
+++
@@ -1,7 +0,0 @@
-#include "connBuffer.h"
-
-using namespace webserver;
-
-const char ConnBuffer::kCRLF[3] = "\r\n";
-int ConnBuffer::kPrependSize_ = 8;
-int ConnBuffer::kInitialSize_ = 1024;
